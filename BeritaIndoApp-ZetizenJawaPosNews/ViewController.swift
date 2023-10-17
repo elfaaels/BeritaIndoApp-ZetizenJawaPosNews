c2fc5f2af57d86b0a2aec894ad710cabede747b4
Elfana, A | Elfaael <elfaael12@gmail.com>
2023-10-17 20:08:55 +0700

    Initial Commit

---
+++ BeritaIndoApp-ZetizenJawaPosNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeritaIndoApp-ZetizenJawaPosNews
+//
+//  Created by Elfana Anamta Chatya on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
